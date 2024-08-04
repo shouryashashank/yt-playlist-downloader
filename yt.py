ebb42da10972c3c88c1a641cc9d5f0072349ab00
Shourya Shashank <shouryashashank@gmail.com>
2024-08-04 20:59:51 +0530

    added many new features

--- yt.py
+++ yt.py
@@ -1,9 +1,26 @@
 import os
 from pytube import Playlist
 from moviepy.editor import *
+from mutagen.easyid3 import EasyID3
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from mutagen.id3 import APIC, ID3
+import urllib.request
+from tqdm import tqdm
 
-
-music_folder_path = "music-yt/"
+file_exists_action=""
+music_folder_path = "music-yt/"   # path to save the downloaded music
+SPOTIPY_CLIENT_ID = "" # Spotify API client ID  # keep blank if you dont need spotify metadata
+SPOTIPY_CLIENT_SECRET = ""  # Spotify API client secret
+try:
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET
+    )
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+except Exception as e:
+    print(f"Failed to connect to Spotify API: {e}")
+    print("Continuing without Spotify API, some song metadata will not be added")
 
 def prompt_exists_action():
     """ask the user what happens if the file being downloaded already exists"""
@@ -12,6 +29,21 @@ def prompt_exists_action():
         return False
     elif file_exists_action == "RA":  # RA == 'Replace All'
         return True
+    
+    print("This file already exists.")
+    while True:
+        resp = (
+            input("replace[R] | replace all[RA] | skip[S] | skip all[SA]: ")
+            .upper()
+            .strip()
+        )
+        if resp in ("RA", "SA"):
+            file_exists_action = resp
+        if resp in ("R", "RA"):
+            return True
+        elif resp in ("S", "SA"):
+            return False
+        print("---Invalid response---")
     
 def make_alpha_numeric(string):
     return ''.join(char for char in string if char.isalnum())
@@ -58,27 +90,107 @@ def download_yt(yt,search_term):
     os.remove(vid_file)
     os.replace(audio_file, f"{music_folder_path}tmp/{yt.title}.mp3")
     audio_file = f"{music_folder_path}tmp/{yt.title}.mp3"
+
     return audio_file
 
+def set_metadata(metadata, file_path):
+    """adds metadata to the downloaded mp3 file"""
+
+    mp3file = EasyID3(file_path)
+
+    # add metadata
+    mp3file["albumartist"] = metadata["artist_name"]
+    mp3file["artist"] = metadata["artists"]
+    mp3file["album"] = metadata["album_name"]
+    mp3file["title"] = metadata["track_title"]
+    mp3file["date"] = metadata["release_date"]
+    mp3file["tracknumber"] = str(metadata["track_number"])
+    mp3file["isrc"] = metadata["isrc"]
+    mp3file.save()
+
+    # add album cover
+    audio = ID3(file_path)
+    with urllib.request.urlopen(metadata["album_art"]) as albumart:
+        audio["APIC"] = APIC(
+            encoding=3, mime="image/jpeg", type=3, desc="Cover", data=albumart.read()
+        )
+    audio.save(v2_version=3)
+
+def search_spotify(search_term : str)->str:
+    """search for the track on spotify"""
+    search_results = sp.search(search_term, type="track", limit=1)
+    if search_results["tracks"]["total"] == 0:
+        return None
+    track = search_results["tracks"]["items"][0]
+    return track["external_urls"]["spotify"]
+
+def get_track_info_spotify(track_url):
+    res = requests.get(track_url)
+    if res.status_code != 200:
+        # retry 3 times
+        for i in range(3):
+            res = requests.get(track_url)
+            if res.status_code == 200:
+                break
+    if res.status_code != 200:
+        print("Invalid Spotify track URL")
+
+    track = sp.track(track_url)
+
+    track_metadata = {
+        "artist_name": track["artists"][0]["name"],
+        "track_title": track["name"],
+        "track_number": track["track_number"],
+        "isrc": track["external_ids"]["isrc"],
+        "album_art": track["album"]["images"][1]["url"],
+        "album_name": track["album"]["name"],
+        "release_date": track["album"]["release_date"],
+        "artists": [artist["name"] for artist in track["artists"]],
+    }
+
+    return track_metadata
+
+def get_track_info_youtube(video):
+
+    track_metadata = {
+        "artist_name": video.author,
+        "track_title":  video.title,
+        "track_number": 0,
+        "isrc": "",
+        "album_art": video.thumbnail_url,
+        "album_name": video.author,
+        "release_date": video.publish_date.strftime("%Y-%m-%d"),
+        "artists": [video.author],
+    }
+
+    return track_metadata
 
 link = input("Enter YouTube Playlist URL: ✨")
 
 yt_playlist = Playlist(link)
 
-folderName = make_alpha_numeric(yt_playlist.title)
-os.mkdir(folderName)
+
 
 totalVideoCount = len(yt_playlist.videos)
 print("Total videos in playlist: 🎦", totalVideoCount)
 
-for index, video in enumerate(yt_playlist.videos, start=1):
-    audio = download_yt(video,video.title)
+for index, video in enumerate(tqdm(yt_playlist.videos), start=1):
+    try:
+        audio = download_yt(video,video.title)
+        if audio:
 
-    # print("Downloading:", video.title)
-    # video_size = video.streams.get_highest_resolution().filesize
-    # print("Size:", video_size // (1024 ** 2), "🗜 MB")
-    # video.streams.get_highest_resolution().download(output_path=folderName)
-    # print("Downloaded:", video.title, "✨ successfully!")
-    # print("Remaining Videos:", totalVideoCount - index)
+                try:
+                    track_url = search_spotify(f"{video.author} {video.title}")
+                except Exception as e:
+                    track_url = None
+                if not track_url:
+                    track_info = get_track_info_youtube(video)
+                else:
+                    track_info = get_track_info_spotify(track_url)
+                set_metadata(track_info, audio)
+                os.replace(audio, f"{music_folder_path}{os.path.basename(audio)}")
+    except Exception as e:
+        print(f"Failed to download {video.title} due to: {e}")
+        continue
 
-print("All videos downloaded successfully! 🎉")
+print("All videos downloaded successfully!")
