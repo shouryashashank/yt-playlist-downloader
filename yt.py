96fc6dae6863aa96e37519e0e9d35f07e9c792bb
Shourya Shashank <shouryashashank@gmail.com>
2024-08-15 15:14:52 +0530

    added gradio ui

--- yt.py
+++ yt.py
@@ -8,19 +8,15 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from mutagen.id3 import APIC, ID3
 import urllib.request
 from tqdm import tqdm
+import gradio as gr
+from tkinter import Tk, filedialog
+from dotenv import load_dotenv
+
 
 file_exists_action=""
 music_folder_path = "music-yt/"   # path to save the downloaded music
-SPOTIPY_CLIENT_ID = "" # Spotify API client ID  # keep blank if you dont need spotify metadata
-SPOTIPY_CLIENT_SECRET = ""  # Spotify API client secret
-try:
-    client_credentials_manager = SpotifyClientCredentials(
-        client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET
-    )
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-except Exception as e:
-    print(f"Failed to connect to Spotify API: {e}")
-    print("Continuing without Spotify API, some song metadata will not be added")
+# SPOTIPY_CLIENT_ID = "" # Spotify API client ID  # keep blank if you dont need spotify metadata
+# SPOTIPY_CLIENT_SECRET = ""  # Spotify API client secret
 
 def prompt_exists_action():
     """ask the user what happens if the file being downloaded already exists"""
@@ -165,32 +161,153 @@ def get_track_info_youtube(video):
 
     return track_metadata
 
-link = input("Enter YouTube Playlist URL: ✨")
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+def folder_select():
+    filename = filedialog.askdirectory()
+    root = Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    if filename:
+        if os.path.isdir(filename):
+            root.destroy()
+            music_folder_path = str(filename)
+            return str(filename)
+        else:
+            root.destroy()
+            music_folder_path = str(filename)
+            return str(filename)
+    else:
+        filename = "Folder not seleceted"
+        root.destroy()
+        music_folder_path = str(filename)
+        return str(filename)
+    
 
-yt_playlist = Playlist(link)
-
-
-
-totalVideoCount = len(yt_playlist.videos)
-print("Total videos in playlist: 🎦", totalVideoCount)
-
-for index, video in enumerate(tqdm(yt_playlist.videos), start=1):
+def downloader(link,exists_action, progress=gr.Progress()):
+    custom_labels = {
+        "Replace all": "RA",
+        "Skip all": "SA",
+        "Determine while downloading from CLI": "",
+    }
+    global file_exists_action 
+    file_exists_action = custom_labels[exists_action]
     try:
-        audio = download_yt(video,video.title)
-        if audio:
-
-                try:
-                    track_url = search_spotify(f"{video.author} {video.title}")
-                except Exception as e:
-                    track_url = None
-                if not track_url:
-                    track_info = get_track_info_youtube(video)
-                else:
-                    track_info = get_track_info_spotify(track_url)
-                set_metadata(track_info, audio)
-                os.replace(audio, f"{music_folder_path}{os.path.basename(audio)}")
+        load_dotenv()
+        SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+        SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+        client_credentials_manager = SpotifyClientCredentials(
+            client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET
+        )
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     except Exception as e:
-        print(f"Failed to download {video.title} due to: {e}")
-        continue
+        print(f"Failed to connect to Spotify API: {e}")
+        print("Continuing without Spotify API, some song metadata will not be added")
+    # link = input("Enter YouTube Playlist URL: ✨")
 
-print("All videos downloaded successfully!")
+    yt_playlist = Playlist(link)
+
+
+
+    totalVideoCount = len(yt_playlist.videos)
+    print("Total videos in playlist: 🎦", totalVideoCount)
+
+    for index, video in enumerate(progress.tqdm(yt_playlist.videos,desc= "downloading..."), start=1):
+        try:
+            print("Downloading: "+video.title)
+            audio = download_yt(video,video.title)
+            if audio:
+
+                    try:
+                        track_url = search_spotify(f"{video.author} {video.title}")
+                    except Exception as e:
+                        track_url = None
+                    if not track_url:
+                        track_info = get_track_info_youtube(video)
+                    else:
+                        track_info = get_track_info_spotify(track_url)
+                    set_metadata(track_info, audio)
+                    os.replace(audio, f"{music_folder_path}{os.path.basename(audio)}")
+        except Exception as e:
+            print(f"Failed to download {video.title} due to: {e}")
+            continue
+
+    print("All videos downloaded successfully!")
+    return "All videos downloaded successfully!"
+
+def save_app_settings(spotify_client_id,spotify_client_secret):
+    try:
+        env_str="SPOTIPY_CLIENT_ID="+spotify_client_id+"\nSPOTIPY_CLIENT_SECRET="+spotify_client_secret
+        with open(".env", "r+") as f:
+            f.write(env_str)
+    except Exception as e:
+        print (e)
+        return "app settings update failed"
+    return "app settings updated"
+
+def ui():
+    with gr.Blocks() as gui:
+        gr.Markdown ("""
+            <table style="border: none; padding: 0;">
+                <tr style="border: none; padding: 0;">
+                    <td style="border: none; padding: 0;">
+                    <a href="https://github.com/Predacons"><img src="https://i.postimg.cc/MKMHRy7Q/acf160a0-f299-45db-ba77-309079191f6b-removebg-preview.png" width="60px" height="60px"></a>
+                    </td>
+                    <td style="border: none; padding: 10; vertical-align: middle;">
+                    <a href="https://github.com/Predacons" style="text-decoration: none;"><H1>Playlist downloader<H1></a>
+                    </td>
+                </tr>
+            </table>""")
+        with gr.Tab(label="Youtube"):
+            gr.Markdown("""
+            <table style="border: none; padding: 0;">
+                <tr style="border: none; padding: 0;" >
+                    <td style="border: none; padding: 0;">
+                    <a href="https://github.com/Predacons"><img src="https://www.gstatic.com/youtube/img/promos/growth/07a2a04c83a46863bd8bb1f316bfcb7ba3d46899d515a605f89a76a254c235d2_244x112.webp" width="160px" height="60px"></a>
+                </tr>
+            </table>""")
+            with gr.Row():
+                image_browse_btn = gr.Button("Select output directory", min_width=1)
+                input_path = gr.Textbox(label="output directory path", scale=5, interactive=False)
+                image_browse_btn.click(folder_select, inputs=[], outputs=input_path, show_progress="hidden")
+            with gr.Row():
+                yt_playlist = gr.Textbox(label="Youtube playlist url")
+            with gr.Row():
+                file_exists_action = gr.Radio(["Replace all", "Skip all", "Determine while downloading from CLI"], label="Song Exist Action", info="What to do when that song already exist in your directory?")
+            with gr.Row():
+                download_btn = gr.Button("Download playlist")
+            with gr.Row():
+                progress_bar = gr.Textbox()
+                download_btn.click(downloader, inputs=[yt_playlist,file_exists_action], outputs=[progress_bar])
+            gr.Markdown ("""
+                        <p style="text-align: center; font-size: small;">Buy <a href="https://www.youtube.com/premium">YouTube Premium</a> to get similar features</a> </p>
+                        """)    
+        with gr.Tab(label="App settings"):
+            gr.Markdown("# App Settings")
+            with gr.Row():
+                SPOTIPY_CLIENT_ID = gr.Textbox(label="Spotify client id")
+                SPOTIPY_CLIENT_SECRET = gr.Textbox(label="Spotify client secret")
+            with gr.Row():
+                save_appsettings_button = gr.Button("Update app settings")
+            with gr.Row():
+                app_settings_update = gr.Textbox(label= "")
+                save_appsettings_button.click(save_app_settings,inputs=[SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET],outputs=app_settings_update)
+
+        gr.Markdown ("""
+                        <p style="text-align: center; font-size: small;">Support artists. Buy music. Attend concerts. 🎵✨</a> </p>
+                        <p style="text-align: center; font-size: small;"><a href="https://github.com/shouryashashank">Shourya Shashank</a> </p>
+                        <p style="text-align: center; font-size: small;">Support me on <a href="https://patreon.com/Predacons?utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=creatorshare_creator&utm_content=join_link">Patreon</a> </p> 
+                     """)    
+
+    return gui
+
+def main():
+    print("Abracadabra!")
+    webapp = ui()
+    webapp.launch()
+    
+
+
+
+if __name__ == "__main__":
+    main()
